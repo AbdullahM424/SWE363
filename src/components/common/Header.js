890f2e7e890bb712c829555afb9a337522223608
Ahmed Alhumaid <humaidakah@gmail.com>
2024-11-08 02:38:32 +0300

    added some routing to the header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import styles from "../../assets/styles/Header.module.css"; 
+import {Link} from "react-router-dom";
 
 
 function Header(){
     return (
         <div className={styles.header}>
           <button className={styles.headerButton}>Facilities</button>
+          <Link to = "/clubs">
           <button className={styles.headerButton}>Clubs</button>
+          </Link>
+         
           <button className={styles.headerButton}>Courses</button>
+          <Link  to = "/instructors">
           <button className={styles.headerButton}>Instructors</button>
+          </Link>
+          
         </div>
       );
 }
