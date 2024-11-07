36e38ed7973c4f95f711b7d04e07e28bde5a100e
Isamil <word7641@gmail.com>
2024-11-08 01:28:38 +0300

    complete singup page

--- src/App.js
+++ src/App.js
@@ -7,19 +7,17 @@ import Instructors from './pages/Instructors.js';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginFormm from './pages/LoginForm.js';
 import SingupForm from './pages/SingupForm.js';
-
 function App() {//the root if the application, sees all the changes in the url
   return (
    <div>
-    {/* <Router>
+    
+    <Router>
       <Routes>
         <Route path = "/" element = {<Home></Home>}> </Route>
         <Route path ="/clubs" element = {<Clubs></Clubs>}></Route>
         <Route path = "/instructors" element={<Instructors></Instructors>}></Route>
       </Routes>
-    </Router> */}
-    <SingupForm />
-
+    </Router> 
     
 
    </div>
