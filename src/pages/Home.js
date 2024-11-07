1e76ab432107622cda282b823fbaced3f6bb802c
Ahmed Alhumaid <humaidakah@gmail.com>
2024-11-08 00:12:57 +0300

    finished

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,7 +29,10 @@ function Home(){
                     <Category title="Clubs" />
                 </Link>
                 <Category title = "Courses"></Category>
-                <Category title = "Instructors"></Category>
+                <Link to="/instructors" style={{ textDecoration: 'none' }}>
+                    <Category title = "Instructors"></Category>
+                </Link>
+                
                 </div>
 
         </div>
