239b50e007117ecf61f5ed2574237594f5b63c12
Isamil <word7641@gmail.com>
2024-11-08 16:48:45 +0300

    first version of css file of the clube profile

--- src/pages/ClubsProfile.js
+++ src/pages/ClubsProfile.js
@@ -4,33 +4,61 @@ import { FaXTwitter } from "react-icons/fa6";
 import React, { useState } from 'react';
 import Announcement from "../components/Announcement";
 import Accordion from 'react-bootstrap/Accordion';
+import { CgProfile } from "react-icons/cg";
+const defaultImg = "../";
+
 
 
 const ClubeProfile = () =>{
+ 
     const [socialMedia, setSocialMedia] = useState({"whatsapp":"", "X":"", "instagram":"" });
     const [name, setName] = useState('');
-    const [descripation, setDescripation] = useState('');
+    const [descripation, setDescripation] = useState(`the descripation of the clube the descripation of the clube the descripation of the clube the descripation 
+                of the clube the descripation of the clube the descripation of the clube the descripation of the clube the descripation of the clube the 
+                descripation of the clube the descripation of the clube the descripation of the clube the descripation of the clube the descripation of 
+                the clube the descripation of the clube the descripation of the clube the descripation of the clube the descripation of the clube the descripation 
+                of the clube the descripation of the clube the descripation of the clube the descripation of the clube the descripation of the clube the descripation 
+                of the clube the descripation of the clube`);
+    const [profileImage, setProfileImage] = useState('');
     const announcementsData = [
         { title: "Announcement 1", content: "This is the first announcement." },
         { title: "Announcement 2", content: "This is the second announcement." },
         { title: "Announcement 3", content: "This is the third announcement." },
+        { title: "Announcement 1", content: "This is the first announcement." },
+        { title: "Announcement 2", content: "This is the second announcement." },
+        { title: "Announcement 3", content: "This is the third announcement." },
+        { title: "Announcement 1", content: "This is the first announcement." },
+        { title: "Announcement 2", content: "This is the second announcement." },
+        { title: "Announcement 3", content: "This is the third announcement." },
+        { title: "Announcement 1", content: "This is the first announcement." },
+        { title: "Announcement 2", content: "This is the second announcement." },
+        { title: "Announcement 3", content: "This is the third announcement." },
+        
       ];
 
 
 return(
-    <div className={style.theBage}>
-
+    <div className={style.thePage}>
+        <div className={style.theBody}>
         <div className={style.card}>
-            <img src="#" alt = "The clube Loge" />
+            <img src="src/assets/images/clubeProfileImg.png"  alt = "The clube Loge" />
             <p className= {style.name} > the name</p>
-            <p className={style.descripation}>the descripation of the clube</p>
+            <p className={style.descripation}>{descripation} </p>
             <a herf = "#"><FaWhatsapp /></a>
             <a herf = "#"><FaInstagram /></a>
             <a herf = "#"><FaXTwitter /></a>
 
         </div>
         <h1>Announcement</h1>
-        <div className={style.card}>
+        <div 
+        className={style.card}
+        style={{
+            maxHeight: '400px',
+            overflowY: 'auto',
+            
+        }}
+    
+        >
         <Accordion>
         {announcementsData.map((item, index) => (
         <Announcement
@@ -44,6 +72,7 @@ return(
       ))}
         </Accordion>
         </div>
+    </div>
     </div>
 )
 }
